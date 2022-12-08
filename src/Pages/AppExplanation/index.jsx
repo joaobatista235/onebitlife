6ac959acaf158c1a0c58797b4df362c03b378e34
joaobatista235 <juniorbatista0404@gmail.com>
2022-12-08 18:36:31 -0300

    aula 1 finalizada

--- src/Pages/AppExplanation/index.jsx
+++ src/Pages/AppExplanation/index.jsx
@@ -1,12 +1,15 @@
-import React from "react";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
-import DefaultButton from "../../Components/Common/DefaultButton";
-import ExplanationCard from "../../Components/Explanation/ExplanationCard";
+import React from "react"
+import { View, Text, StyleSheet, ScrollView } from "react-native"
+import DefaultButton from "../../Components/Common/DefaultButton"
+import ExplanationCard from "../../Components/Explanation/ExplanationCard"
+import { useNavigation } from "@react-navigation/native"
 
 export default function AppExplanation() {
 
-    function handleSetShowHome() {
-        console.log("Botão clicado!");
+    const navigation = useNavigation()
+
+    function handleNavHome() {
+        navigation.navigate("Home")
     }
 
     return (
@@ -26,7 +29,7 @@ export default function AppExplanation() {
                     </Text>
                     <DefaultButton
                         buttonText={"Continuar"}
-                        handlePress={handleSetShowHome}
+                        handlePress={handleNavHome}
                         width={250}
                         height={50}
                     />
